3506f00d12b2248069826143c9cac89e9a678110
Brendh4 <brendhauchiha@gmail.com>
2024-01-24 14:00:04 +0000

    adds to read component

--- bookbuddy/src/App.jsx
+++ bookbuddy/src/App.jsx
@@ -4,15 +4,19 @@ import StickyNav from 'react-sticky-nav';
 import BookList from './components/BookList';
 import FavoritesList from './components/FavouritesList';
 import SearchBar from './components/SearchBar';
+import ToReadList from './components/ToReadList';
 
 function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [favorites, setFavorites] = useState([]);
+  const [toRead, setToRead] = useState([]);
 
   // Load favorites from local storage on component mount
   useEffect(() => {
     const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    const storedToRead = JSON.parse(localStorage.getItem('toRead')) || [];
     setFavorites(storedFavorites);
+    setToRead(storedToRead);
   }, []);
 
   // Handler for search functionality
@@ -42,9 +46,24 @@ function App() {
     localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
   };
 
+  // Function to add a book to ToRead
+  const addToToRead = (book) => {
+    const updatedToRead = [...toRead, book];
+    setToRead(updatedToRead);
+    localStorage.setItem('toRead', JSON.stringify(updatedToRead));
+  };
+
+  // Function to remove a book from ToRead
+  const removeFromToRead = (book) => {
+    const updatedToRead = toRead.filter((item) => item.id !== book.id);
+    setToRead(updatedToRead);
+    localStorage.setItem('toRead', JSON.stringify(updatedToRead));
+  };
+
+
   return (
     <div className="container mt-4">
-      <StickyNav>
+      <StickyNav style={{ top: 0, zIndex: 1000 }}>
         <div className="row align-items-center justify-content-center">
           <div className="col-md-4 text-center">
             <h1>BookBuddy</h1>
@@ -55,13 +74,17 @@ function App() {
         </div>
       </StickyNav>
 
-      <div className="row">
+      <div className="row" style={{ marginTop: '80px' }}>
         <div className="col-md-4">
           <BookList books={searchResults} onAddToFavorites={addToFavorites} />
         </div>
 
         <div className="col-md-4">
           <FavoritesList favorites={favorites} onRemoveFromFavorites={removeFromFavorites} />
+        </div>
+
+        <div className="col-md-4">
+          <ToReadList toRead={toRead} onRemoveFromToRead={removeFromToRead} />
         </div>
       </div>
     </div>
